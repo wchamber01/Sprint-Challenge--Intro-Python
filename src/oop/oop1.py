30b6a6e594f47af376fc84314720b705d8b6b1cc
wchamber01 <williamchambers@gmail.com>
2020-04-09 23:19:39 -0400

    initial commit  with oop1 completed

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,25 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle:
+    '''base class - Vehicle'''
+    pass
+class FlightVehicle(Vehicle):
+    '''base class for Airplane and Starship'''
+    pass
+class Starship(FlightVehicle):
+    '''child class'''
+    pass
+class Airplane(FlightVehicle):
+    '''child class'''
+    pass
+class GroundVehicle(Vehicle):
+    '''base class for Car and Motorcycle'''
+    pass
+class Car(GroundVehicle):
+    '''child class'''
+    pass
+class Motorcycle(GroundVehicle):
+    '''child class'''
+    pass
